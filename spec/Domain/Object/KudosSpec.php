5cc3839fbbe01c94648a7726ae729ce7fae24d80
Arnel Labarda <aqlx86@gmail.com>
2016-03-28 20:42:08 +0800

    fix kudos object spec

--- spec/Domain/Object/KudosSpec.php
+++ spec/Domain/Object/KudosSpec.php
@@ -11,4 +11,25 @@ class KudosSpec extends ObjectBehavior
     {
         $this->shouldHaveType('Domain\Object\Kudos');
     }
+
+    function it_can_increase_the_kudos_count()
+    {
+        $this->increase_by(1);
+    }
+
+    function it_can_get_the_kudos_count()
+    {
+        $this->get_count()
+            ->shouldReturn(0);
+    }
+
+    function it_can_get_3_kudos_count()
+    {
+        $this->it_can_increase_the_kudos_count();
+        $this->it_can_increase_the_kudos_count();
+        $this->it_can_increase_the_kudos_count();
+
+        $this->get_count()
+            ->shouldReturn(3);
+    }
 }
