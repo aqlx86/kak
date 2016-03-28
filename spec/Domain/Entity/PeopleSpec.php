90690ca8f7ce87d63ad48c2076e75fb1fcc321d9
Arnel Labarda <aqlx86@gmail.com>
2016-03-28 20:40:45 +0800

    fix people entity spec

--- spec/Domain/Entity/PeopleSpec.php
+++ spec/Domain/Entity/PeopleSpec.php
@@ -11,4 +11,24 @@ class PeopleSpec extends ObjectBehavior
     {
         $this->shouldHaveType('Domain\Entity\People');
     }
+
+    function it_has_an_id()
+    {
+        $this->id->shouldBe(null);
+    }
+
+    function it_has_an_email()
+    {
+        $this->email->shouldBe(null);
+    }
+
+    function it_has_a_username()
+    {
+        $this->username->shouldBe(null);
+    }
+
+    function it_has_a_password()
+    {
+        $this->password->shouldBe(null);
+    }
 }
--- src/Domain/Entity/People.php
+++ src/Domain/Entity/People.php
@@ -4,4 +4,13 @@ namespace Domain\Entity;
 
 class People
 {
+    public $id;
+    public $username;
+    public $email;
+    public $password;
+
+    public function hash_password()
+    {
+        return bcrypt($this->password);
+    }
 }
